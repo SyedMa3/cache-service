25860de8441fc536460ef842170957caddc3501d
Syed Abdul Mateen <72622483+SyedMa3@users.noreply.github.com>
2022-06-20 19:12:10 +0530

    Init rpc service (#1)
    * Generated code from proto
    
    * Added Get and Set response methods
    
    * added client.go
    
    * few minor changes

---
+++ client/client.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/SyedMa3/cache-service/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const serverAddr = "localhost:9000"
+
+func testConn(client pb.RpcServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	resp, err := client.Get(ctx, &pb.GetRequest{Key: "test"})
+	if err != nil {
+		log.Fatalf("client.Get Failed!")
+	}
+	log.Printf("client.Get response: %s", resp)
+
+	resp, err = client.Set(ctx, &pb.SetRequest{Key: "test", Value: []byte("testvalue")})
+	if err != nil {
+		log.Fatalf("client.Set Failed!")
+	}
+	log.Printf("client.Set response: %s", resp)
+}
+
+func main() {
+
+	conn, err := grpc.Dial(serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Could not connect at %v", serverAddr)
+	}
+	defer conn.Close()
+
+	client := pb.NewRpcServiceClient(conn)
+
+	testConn(client)
+}
---
+++ server/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/SyedMa3/cache-service/proto"
+
+	"google.golang.org/grpc"
+)
+
+type rpcServer struct {
+	pb.UnimplementedRpcServiceServer
+}
+
+func (s *rpcServer) Get(ctx context.Context, in *pb.GetRequest) (*pb.Response, error) {
+	return &pb.Response{Value: []byte("Mateen will implement me")}, nil
+}
+
+func (s *rpcServer) Set(ctx context.Context, in *pb.SetRequest) (*pb.Response, error) {
+	return &pb.Response{Value: []byte("not implemented yet. Mateen will implement me")}, nil
+}
+
+func newServer() *rpcServer {
+	s := &rpcServer{}
+	return s
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen at port 9000")
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterRpcServiceServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+}
